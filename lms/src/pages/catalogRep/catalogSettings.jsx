16d0d25a7ac0a527b99baa74cfad475df70d99c3
miguelako029 <miguellmilanez@gmail.com>
2023-07-10 11:57:09 +0800

    updated

--- lms/src/pages/catalogRep/catalogSettings.jsx
+++ lms/src/pages/catalogRep/catalogSettings.jsx
@@ -4,6 +4,8 @@ import SideBar from "../../components/sidebar/sidebar";
 import { Typography, Box } from "@mui/material";
 import TopBar from "../../components/topbar/topbar";
 
+import TextField from "@mui/material/TextField";
+
 export default function books() {
   return (
     <>
@@ -15,6 +17,15 @@ export default function books() {
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
           {" "}
           <h1>Catalog list</h1>
+          <TextField id="outlined-basic" label="Catalog name" fullWidth />
+          <TextField
+            id="outlined-multiline-static"
+            label="Description"
+            multiline
+            rows={4}
+            sx={{ marginTop: "20px" }}
+            fullWidth
+          />
         </Box>
       </Box>
     </>
