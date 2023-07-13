2af133e992640c5aaa913df52867dcf3892fcabe
miguelako029 <miguellmilanez@gmail.com>
2023-07-13 11:05:28 +0800

    updated

--- lms/src/pages/catalogRep/catalogSettings.jsx
+++ lms/src/pages/catalogRep/catalogSettings.jsx
@@ -1,33 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBar from "../../components/sidebar/sidebar";
-
 import { Typography, Box } from "@mui/material";
 import TopBar from "../../components/topbar/topbar";
-
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+import { Checkbox, Button } from "@mui/material";
 import CatTable from "../catalogRep/categoryTable";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+export default function Books() {
+  const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
 
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
+  const handleCheckboxChange = isChecked => {
+    setIsCheckboxChecked(isChecked);
+  };
 
-export default function books() {
   return (
     <>
       <TopBar />
@@ -36,7 +21,6 @@ export default function books() {
         <SideBar />
 
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          {" "}
           <h1>Catalog list</h1>
           <TextField id="outlined-basic" label="Catalog name" fullWidth />
           <TextField
@@ -47,35 +31,41 @@ export default function books() {
             sx={{ marginTop: "20px" }}
             fullWidth
           />
-          <Button
-            variant="outlined"
-            sx={{
-              marginTop: "20px",
-            }}
-          >
-            Add Category
-          </Button>
-          <Button
-            variant="contained"
-            color="success"
-            sx={{
-              marginTop: "20px",
+          {!isCheckboxChecked && (
+            <Button
+              variant="outlined"
+              sx={{
+                marginTop: "20px",
+              }}
+            >
+              Add Category
+            </Button>
+          )}
 
-              border: "0px solid",
-            }}
-          >
-            Save
-          </Button>
-          <Button
-            variant="outlined"
-            color="error"
-            sx={{
-              marginTop: "20px",
-            }}
-          >
-            Cancel
-          </Button>
-          <CatTable />
+          {isCheckboxChecked && (
+            <Button
+              variant="contained"
+              color="success"
+              sx={{
+                marginTop: "20px",
+              }}
+            >
+              Save
+            </Button>
+          )}
+          {isCheckboxChecked && (
+            <Button
+              variant="outlined"
+              color="error"
+              sx={{
+                marginTop: "20px",
+                marginLeft: "5px",
+              }}
+            >
+              Cancel
+            </Button>
+          )}
+          <CatTable onCheckboxChange={handleCheckboxChange} />
         </Box>
       </Box>
     </>
--- lms/src/pages/catalogRep/categoryTable.js
+++ lms/src/pages/catalogRep/categoryTable.js
@@ -1,3 +1,6 @@
+import React, { useState, useEffect } from "react";
+import { db } from "../../firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -5,28 +8,39 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+export default function Category({ onCheckboxChange }) {
+  const [rows, setRows] = useState([]);
+  const [isButtonVisible, setButtonVisible] = useState(false);
 
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
+  // Fetch data from Firestore and update the rows state
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "categories"));
+      const data = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setRows(data);
+    };
 
-export default function books() {
+    fetchData();
+  }, []);
+
+  const handleCheckboxChange = event => {
+    const isChecked = event.target.checked;
+    onCheckboxChange(isChecked);
+    setButtonVisible(isChecked);
+  };
+
   return (
     <>
       <TableContainer component={Paper} sx={{ marginTop: 5 }}>
         <Table fullWidth aria-label="simple table">
           <TableHead>
             <TableRow>
+              <TableCell></TableCell> {/* Empty cell for checkbox */}
               <TableCell>Category Name</TableCell>
               <TableCell align="right">Details</TableCell>
               <TableCell align="right">Created</TableCell>
@@ -34,17 +48,18 @@ export default function books() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map(row => (
+            {rows.map(data => (
               <TableRow
-                key={row.name}
+                key={data.id}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
-                  {row.name}
+                  <Checkbox onClick={handleCheckboxChange} />
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell>{data.category_name}</TableCell>
+                <TableCell align="right">{data.description}</TableCell>
+                <TableCell align="right">{data.created}</TableCell>
+                <TableCell align="right">{data.updated}</TableCell>
               </TableRow>
             ))}
           </TableBody>
