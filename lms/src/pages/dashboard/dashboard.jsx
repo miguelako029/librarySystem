4dd07b711282c42e978907dd5bcfb2b48a9efe38
miguelako029 <miguellmilanez@gmail.com>
2023-09-26 12:39:56 +0800

    update

--- lms/src/pages/dashboard/dashboard.jsx
+++ lms/src/pages/dashboard/dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { auth } from "../../firebase-config";
 import SideBar from "../../components/sidebar/sidebar";
 import TopBar from "../../components/topbar/topbar";
@@ -11,12 +11,17 @@ import {
   doc,
   setDoc,
   getDoc,
+  addDoc,
   increment,
   runTransaction,
 } from "firebase/firestore";
 import { db, fs } from "../../firebase-config";
 
+import { AuthContextProvide } from "../../components/context/AuthenticatorContext";
+
 export default function Dashboard() {
+  const { currentUser } = useContext(AuthContextProvide);
+
   const [selectedCatalogId, setSelectedCatalogId] = useState(null);
   const [uid, setUid] = useState(null);
 
@@ -32,32 +37,27 @@ export default function Dashboard() {
     return () => unsubscribe();
   }, []);
 
-  const SelectedBook = async (book) => {
+  const SelectedBook = (book) => {
+    console.log(uid);
     if (uid) {
-      try {
-        // Construct the reference to the user's cart document with the user's UID
-        const cartCollection = collection(db, `cart-${uid}`);
-        const cartBookRef = doc(cartCollection, book.id);
+      // Construct the reference to the user's cart document with the user's UID
 
-        // Extract the ID from the document reference
-        const cartBookId = cartBookRef.id;
-        console.log("Cart Book ID:", cartBookId);
+      const cartCollection = collection(db, "cart");
+      const cartBookRef = doc(cartCollection);
 
-        // Check if the document exists before setting it
-        const cartBookDoc = await getDoc(cartBookRef);
-
-        if (cartBookDoc.exists()) {
-          console.log("Book is already in the cart");
-        } else {
-          // Set the book data in the cart document
-          await setDoc(cartBookRef, {
-            book: book.id,
-          });
+      // Set the book data in the cart document
+      setDoc(cartBookRef, {
+        book: book.id,
+        bookQty: 1,
+        status: "ongoing",
+        userId: currentUser.uid,
+      })
+        .then(() => {
           console.log("Added to cart");
-        }
-      } catch (error) {
-        console.error("Error adding to cart:", error);
-      }
+        })
+        .catch((error) => {
+          console.error("Error adding to cart:", error);
+        });
     } else {
       console.log("Error: User not authenticated");
     }
